39da7ba6deb79e0a4cc6a4263a16faa1ea947d90
Silvia García Cacho <silvia@garca.info>
2018-12-10 18:38:20 +0100

    Do features from ticket #1

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,39 @@
 'use strict';
 
-/* Generamos un número al azar (const = math.random) y lo mostramos en consola */
-
-const randomNumber = Math.ceil((Math.random()*100)+1);
-console.log(randomNumber);
-
 const btnSelector = document.querySelector('.form__btn');
 const inputSelector = document.querySelector('#numberInput');
-const hintSelector = document.querySelector('.container__hint');
+const hintSelector = document.querySelector('.container__future-hint');
 const counterSelector = document.querySelector('.container__counter');
 counterSelector.innerHTML = 0;
 
-function guessNumber(event){
-    event.preventDefault();
+/* Generamos un número al azar (const = math.random) y lo mostramos en consola */
+function randomNumber(max){
 
+    return Math.ceil((Math.random()*max)+1);
+
+}
+const r = randomNumber(99); 
+console.log(r);
+
+function guessNumber(event){
+
+    event.preventDefault();
     const givenNumber = Number(inputSelector.value);
-    // mostramos el número en consola y
+    // mostramos el número en consola 
     console.log(givenNumber);
-    hintSelector.classList.remove('hidden');
-    // comprobamos si su número coincide con el que hemos generado
-    // si no: 
-    if(givenNumber !== randomNumber){
-        // añadimos un número al contador (++)
-        counterSelector.innerHTML ++ ;
-        // abajo sale un mensaje "demasiado alto" o "demasiado bajo"
-        if(givenNumber > randomNumber){
-            hintSelector.innerHTML = "<p>demasiado alto</p>";
+    // añadimos un número al contador (++)
+    counterSelector.innerHTML ++ ;
+    // comprobamos si el número introducido coincide con el random o no
+    function guessMessage(){
+        if(givenNumber > r){
+            hintSelector.innerHTML = '<div class="container__hint"><p>demasiado alto</p></div>';
+        } else if(givenNumber < r) {
+            hintSelector.innerHTML = '<div class="container__hint"><p>demasiado bajo</p></div>';
         } else {
-            hintSelector.innerHTML = "<p>demasiado bajo</p>";
+        hintSelector.innerHTML = '<div class="container__hint-success"><p>¡HAS GANADO, CAMPEONA!</p>';
         }
-        // si sí: abajo sale un mensaje de "has ganado campeona"
-    } else {
-        hintSelector.innerHTML = "<p>¡HAS GANADO, CAMPEONA!</p>";
     }
+    guessMessage();
 
 }
 
